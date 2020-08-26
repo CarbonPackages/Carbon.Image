3c90f207f9672999e2b2ea1b4eb17e16c0999588
Jon Uhlmann <me@jonnitto.ch>
2020-08-26 17:35:56 +0200

    ✨ Provide Assets for modern and old browsers

--- Resources/Private/Assets/Oldie.Main.js
+++ Resources/Private/Assets/Oldie.Main.js
@@ -3,6 +3,22 @@ import "lazysizes/plugins/parent-fit/ls.parent-fit";
 import "lazysizes/plugins/aspectratio/ls.aspectratio";
 import "lazysizes/plugins/native-loading/ls.native-loading";
 
+function loadJS(url) {
+    let script = document.createElement("script");
+    script.src = url;
+    script.defer = true;
+    document.head.appendChild(script);
+}
+
+if ("objectFit" in document.documentElement.style === false) {
+    loadJS("/_Resources/Static/Packages/Carbon.Image/Scripts/Pollyfill.ObjectFit.js");
+}
+
+if (!window.HTMLPictureElement) {
+    document.createElement("picture");
+    loadJS("/_Resources/Static/Packages/Carbon.Image/Scripts/Pollyfill.Picture.js");
+}
+
 lazySizes.cfg.nativeLoading = {
     setLoadingAttribute: true,
     disableListeners: {
